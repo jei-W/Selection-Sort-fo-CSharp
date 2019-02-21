41bb9994d772e928f1a352d620592f117c6e1c41
jei-W <gkdtkdv123@naver.com>
2019-02-22 01:37:42 +0900

    Selection Sort의 구현 == *오름차순 정렬 구현 *내림차순 정렬 구현

---
+++ Sorting Algorithm/MainApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithm
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            int[] arg = { 2, 8, 5, 9, 1, 13, 7 };
+
+            Print();
+
+            SelectionSort.AscendingSort(arg);
+            Print();
+
+            SelectionSort.DescendingSort(arg);
+            Print();
+
+
+            void Print()
+            {
+                foreach( int i in arg )
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ Sorting Algorithm/SelectionSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithm
+{
+    class SelectionSort
+    {
+        public static void AscendingSort( int[] args )
+        {
+            for( int i = 0; i < args.Length; i++ )
+            {
+                int smallestIndex = i;
+
+                for( int j = i + 1; j < args.Length; j++ )
+                {
+                    if( args[smallestIndex] > args[j] )
+                    {
+                        smallestIndex = j;
+                    }
+                }
+
+                Swap(ref args[i], ref args[smallestIndex]);
+            }
+        }
+
+        public static void DescendingSort( int[] args )
+        {
+            for( int i = 0; i < args.Length; i++ )
+            {
+                int biggestIndex = i;
+
+                for( int j = i + 1; j < args.Length; j++ )
+                {
+                    if( args[biggestIndex] < args[j] )
+                    {
+                        biggestIndex = j;
+                    }
+                }
+
+                Swap(ref args[i], ref args[biggestIndex]);
+            }
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
